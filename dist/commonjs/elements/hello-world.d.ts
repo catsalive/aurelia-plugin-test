8f19ec0591c40cc144fae30d07746ca66c059b77
Griffith Thomas <gthomas.1978@gmail.com>
2019-10-22 18:42:53 -0700

    added action

--- dist/commonjs/elements/hello-world.d.ts
+++ dist/commonjs/elements/hello-world.d.ts
@@ -1,4 +1,8 @@
+import { Store } from 'aurelia-store';
 export declare class HelloWorld {
+    protected store: Store<any>;
     message: string;
     state: any;
+    constructor(store: Store<any>);
+    attached(): void;
 }
--- src/elements/hello-world.ts
+++ src/elements/hello-world.ts
@@ -1,9 +1,22 @@
 import {bindable} from 'aurelia-framework';
-import { connectTo } from 'aurelia-store';
-import { pluck } from 'rxjs/operators';
+import { connectTo, Store } from 'aurelia-store';
+
+const demoAction = async (state, frameworkName) => {
+  const newState = Object.assign({}, state);
+  newState.frameworks = [...newState.frameworks, frameworkName];
+  return newState;
+}
 
 @connectTo()
 export class HelloWorld {
   @bindable public message: string = '';
   state: any;
+
+  constructor(protected store: Store<any>) {
+    this.store.registerAction('DemoAction', demoAction);
+  }
+
+  attached() {
+    this.store.dispatch(demoAction, 'Sheesh')
+  }
 }
